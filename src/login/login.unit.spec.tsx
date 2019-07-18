05486d9319aa5ab5a6ce32a50dcf0d8fd4385780
Tanay PrabhuDesai <tanayseven@gmail.com>
2019-07-18 12:33:13 -0400

    Recreate the project with create-react-app

--- src/login/login.unit.spec.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react"
-
-describe(`Login Component`, function () {
-    it(`just a sample test used as filler`, function () {
-        expect(true).toEqual(true)
-    })
-})
--- stub-server/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = () => {
-    const data = {users: []}
-    for (let i = 0 ; i < 1000 ; i++) {
-        data.users.push({id: i, name: `user${i}`})
-    }
-    return data
-}
