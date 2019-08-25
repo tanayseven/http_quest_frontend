7332926d32293591ef6977263f92aed7ec4da890
Tanay PrabhuDesai <tanayseven@gmail.com>
2019-08-25 14:57:40 +0530

    Add ReactRouter support and add ESLint support

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {HashRouter, Route} from 'react-router-dom'
+import './App.css'
+import {HomePage, LoginPage} from "./pages";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <HashRouter hashType='hashbang'>
+            <Route exact path='/' component={LoginPage} />
+            <Route path='/home' component={HomePage} />
+        </HashRouter>
+      </div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/login.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import {Link} from "react-router-dom";
+
+export const LoginPage = () => {
+   return <>
+      <p>Login Page</p>
+      <Link to='/home'>Home</Link>
+   </>
+}
