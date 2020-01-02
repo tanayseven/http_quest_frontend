93e61c4adcd31e50a6523764e0bee66b741e27da
Tanay PrabhuDesai <tanayseven@gmail.com>
2020-01-02 23:21:28 +0000

    Force build images even though they exist

--- shell-scripts/publish-code-coverage.sh
+++ shell-scripts/publish-code-coverage.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 aws s3 sync s3://tanayseven-http-quest-frontend-build/$TRAVIS_BUILD_NUMBER/coverage_files/ .
-docker-compose run publish-coverage
+docker-compose run publish-coverage --build
