fa3e7699e90c4953e86d6a0646c8cd14d4351ac9
Tanay PrabhuDesai <tanayseven@gmail.com>
2020-01-05 19:41:27 +0000

    Build correct images

--- shell-scripts/publish-code-coverage.sh
+++ shell-scripts/publish-code-coverage.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 aws s3 sync s3://tanayseven-http-quest-frontend-build/$TRAVIS_BUILD_NUMBER/coverage_files/ . || exit 1
-docker-compose build || exit 1
+docker-compose build publish-coverage || exit 1
 docker-compose run publish-coverage || exit 1
